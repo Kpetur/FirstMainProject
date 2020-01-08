16d6be85c22690074f381dae750e7004c84bdd0b
Kpetur <kosziklapetur@yahoo.com>
2020-01-08 10:52:48 +0200

    Tema lab 9

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
 public class Main {
 
+
     public static void main(String[] args) {
         System.out.println("Hello World" );
-        System.out.println("Merry Christmass");
+
+        String name = "Peter";
+        System.out.println(name);
+
+        int x = 5;
+        int y = 3;
+        int sum = x+y;
+        System.out.println(sum);
+
+        float z = 1.5f;
+        double imp = x/z;
+        System.out.println(imp);
+
+        int a = -5 + 8 * 6;
+        System.out.println(a);
+
+        int b = (55 + 9) % 9;
+        System.out.println(b);
+
+        float c = 20 + ((-3 * 5) / 8f);
+        System.out.println(c);
+
+        int d = 5 + ((15/3) * 2) - (8 % 3);
+        System.out.println(d);
+
     }
 }
